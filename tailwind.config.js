45638f482c040d86c5bf8a99ac380cf11ca42a0e
Jeremy Lin <jeremy60572@gmail.com>
2025-12-27 02:48:06 -0800

    Added packages for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
     extend: {
       animation: {
         'float': 'float 1.5s ease-in-out infinite',
+        'star-movement-bottom': 'star-movement-bottom linear infinite alternate',
+        'star-movement-top': 'star-movement-top linear infinite alternate',
       },
       keyframes: {
         'float': {
@@ -16,6 +18,14 @@ module.exports = {
           '50%': { 
             transform: 'scale(1.07)',
           },
+        },
+        'star-movement-bottom': {
+          '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+          '100%': { transform: 'translate(-100%, 0%)', opacity: '0' },
+        },
+        'star-movement-top': {
+          '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+          '100%': { transform: 'translate(100%, 0%)', opacity: '0' },
         },
       },
     },
