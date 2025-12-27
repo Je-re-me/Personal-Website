0ed1f88750ff3bb806715e47940025a1bda7b847
Jeremy Lin <jeremy60572@gmail.com>
2025-12-27 02:49:26 -0800

    Added Navbar border component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import StarBorder from './StarBorder'
 import "../styles/styles.css";
 
 export default function Navbar() {
@@ -6,21 +7,53 @@ export default function Navbar() {
     <a href="#home" className="home-page">
       <img src="/Logo.png" alt="Home Icon" className="logo hover:animate-float hover:drop-shadow-[0_0_20px_rgba(96,165,250,0.8)]" ></img>
     </a>
+    
     <ul>
-      <li className="hover:border-white-400 hover:shadow-[0_0_15px_rgba(96,165,250,0.6)] transition-all duration-300">
-        <a href="#contact">Contact</a>
+      <li>
+        <StarBorder
+          as="a"
+          href="#contact"
+          color="#7484BE"
+          speed="5s"
+          className="block"
+        >
+          Contact
+        </StarBorder>
       </li>
-      <li className="hover:border-white-400 hover:shadow-[0_0_15px_rgba(96,165,250,0.6)] transition-all duration-300">
-        <a href="#projects">Projects</a>
+      <li>
+        <StarBorder
+          as="a"
+          href="#projects"
+          color="#7484BE"
+          speed="5s"
+          className="block"
+        >
+          Projects
+        </StarBorder>
       </li>
-      <li className="hover:border-white-400 hover:shadow-[0_0_15px_rgba(96,165,250,0.6)] transition-all duration-300">
-        <a href="#about">About</a>
+      <li>
+        <StarBorder
+          as="a"
+          href="#about"
+          color="#7484BE"
+          speed="5s"
+          className="block"
+        >
+          About
+        </StarBorder>
       </li>
-      <li className="hover:border-white-400 hover:shadow-[0_0_15px_rgba(96,165,250,0.6)] transition-all duration-300">
-        <a href="#resume">Resume</a>
+      <li>
+        <StarBorder
+          as="a"
+          href="#resume"
+          color="#7484BE"
+          speed="5s"
+          className="block"
+        >
+          Resume
+        </StarBorder>
       </li>
     </ul>
-  
-  
+    
   </nav>
 }
--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import "../styles/styles.css";
+import CircularText from '../components/CircularText.jsx';
+import ScrollFloat from '../components/ScrollFloat.jsx'
 
 export default function Page() {
   return (
     <div className="page">
-      <section id="home">
-        <h1>Home</h1>
-        <p>Welcome to my personal website!</p>
+      <section id="home" className="flex items-center justify-center">
+        <div className="relative inline-block">
+          <CircularText
+            text="2ND YEAR COMP SCI * ASPIRING DEVELOPER * FULLSTACK * CYBERSECURITY * "
+            onHover="slowDown"
+            spinDuration={25}
+            className="circular-text"
+          />
+          <div className="absolute top-1/2 left-1/2 pointer-events-none" style={{ transform: 'translate(-50%, -40%)' }}>
+            <h1 className="text-white text-center" style={{ fontSize: '6rem', letterSpacing: '0.05rem', fontWeight: 400, lineHeight: 0.9, margin: 0 }}>
+              JEREMY
+            </h1>
+            <div style={{ width: '100%', height: '2px', backgroundColor: 'white', margin: '0.5rem 0' }}></div>
+            <ScrollFloat
+              animationDuration={1.7}
+              ease='back.inOut(1.5)'
+              stagger={0.05}
+              containerClassName="text-center"
+              containerStyle={{ margin: 0 }}
+              textClassName="text-white"
+              style={{ fontSize: '6rem', letterSpacing: '0.05rem', fontWeight: 400, lineHeight: 0.9 }}
+            >
+              LIN
+            </ScrollFloat>
+          </div>
+        </div>
       </section>
 
       <section id="contact">
