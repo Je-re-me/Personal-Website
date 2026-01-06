2ad453a39f3e7af7a6f34b6f22fa2f7f0b2d958b
Jeremy Lin <jeremy60572@gmail.com>
2026-01-06 12:18:23 -0800

    edited text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,18 +13,12 @@ export default function AboutMe() {
                 speed="5s"
             >
                 <ScrollReveal
-                    baseOpacity={0}
+                    baseOpacity={50}
                     enableBlur={true}
                     baseRotation={5}
                     blurStrength={10}
                     >
-                    I’m currently pursuing a Bachelor of Science in Computer Science at the University of British Columbia, with an expected graduation date of May 2028. 
-                    I’m an aspiring developer passionate about breaking into the tech industry, with a strong interest in cybersecurity and AI development. 
-                    I love playing badminton in my spare time, and I’d be happy to connect with others who share similar interests or are passionate about tech and innovation.
-
-                    You can see all of my projects on GitHub: https://github.com/Je-re-me
-
-                    If you have any questions, please reach out to my email: Jeremy60572@gmail.com
+                    kill yourself
                 </ScrollReveal>
             </AboutBorder>
         </div>
