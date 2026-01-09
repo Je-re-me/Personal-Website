bdf091261f34f9cd08e9c3cb3a5f6e9cf30a13dc
Jeremy Lin <jeremy60572@gmail.com>
2026-01-09 13:51:11 -0800

    Added resume link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,16 @@ import StarBorder from './StarBorder'
 import "../styles/styles.css";
 
 export default function Navbar() {
+  const handleResumeClick = (e) => {
+    e.preventDefault();
+    const link = document.createElement('a');
+    link.href = `${process.env.PUBLIC_URL}/Resume_Jeremy_L.pdf`;
+    link.download = 'Resume_Jeremy_L.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return <nav className="navbar"> 
     <a href="#home" className="home-page">
       <img src="/Logo.png" alt="Home Icon" className="logo hover:animate-float hover:drop-shadow-[0_0_20px_rgba(96,165,250,0.8)]" ></img>
@@ -45,7 +55,8 @@ export default function Navbar() {
       <li>
         <StarBorder
           as="a"
-          href="#resume"
+          href="#"
+          onClick={handleResumeClick}
           color="#7484BE"
           speed="5s"
           className="block"
